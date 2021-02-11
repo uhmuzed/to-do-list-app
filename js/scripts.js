1b07e5fb33983d2a467b7730505fcf467693e730
uhmuzed <75945396+uhmuzed@users.noreply.github.com>
2021-02-11 00:42:47 -0600

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,12 @@ function newItem() {
   let inputValue = $("#input").val();
   li.append(document.createTextNode(inputValue));
 
-  if (!inputValue) {
+
+  if (!inputValue) { //if inputValue is falsy
     alert("You must write something");
   } else {
     $("#list").append(li);
-    console.log(`${"Added "} "${inputValue}"`);
+    console.log(`${"Added "} "${inputValue}"`); //adding responsive feedback
   }
 
   //1a. clear list for new inputValue
@@ -17,7 +18,7 @@ function newItem() {
   //2. crossing out item from List
   li.on('dblclick', function (e) {
         li.toggleClass('strike');
-        console.log(`${"Completed "} "${inputValue}"`);
+        console.log(`${"Completed "} "${inputValue}"`); //feedback in console
   });
 
   //3a. adding the delete button
@@ -27,7 +28,7 @@ function newItem() {
   //3b. add class delete
   crossOutButton.on("click", function() {
     li.addClass("delete");
-    console.log(`${"Deleted "} "${inputValue}"`);
+    console.log(`${"Deleted "} "${inputValue}"`); //feedback in console
   });
 
   //4. reordering List
